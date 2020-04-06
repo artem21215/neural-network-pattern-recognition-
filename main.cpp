b5da8b9e21ed7d3462ef9cd6c4a37872c5a13db9
Artem <13_22_54@mail.ru>
2020-04-06 13:10:28 +0700

    Add a function printpixel

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "input.cpp"
-int input(char s[6], int arr[]);
+void printpixel(int arr[], int n);
 int main()
 {
-    int* a;
-    input("2.bmp",a);
-//	printf("%d ",a[10]);
+    int *a, kpixel = 0;
+    a = input("2.bmp", a, &kpixel);
+    printpixel(a, kpixel);
     return 0;
+}
+void printpixel(int arr[], int n)
+{
+    puts("");
+    for (int kk = 0; kk < n; kk++) {
+        printf("%d ", arr[kk]);
+        if (kk % 10 == 0)
+            printf("\n%d - ", kk);
+    }
+    puts("");
 }
