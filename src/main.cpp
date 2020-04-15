0449319552b0dd8b31e7be715e92c4b30e94fe36
Artem <13_22_54@mail.ru>
2020-04-15 21:02:25 +0700

    Return set of main.c

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include "network.h"
 int main()
 {
- int *a, kpixel = 0;
- 		   a = input("files_bmp/2.bmp", a, &kpixel);
-    	printpixel(a, kpixel);
+    int *a, kpixel = 0;
+    a = input("files_bmp/2.bmp", a, &kpixel);
+    printpixel(a, kpixel);
     return 0;
 }
