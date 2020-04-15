d691c81d7b4293c2176db7f00f46917e44bfd69d
Artem <13_22_54@mail.ru>
2020-04-15 20:28:33 +0700

    Add check clang format with travis,print to README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # neural-network-pattern-recognition-
+[![Build Status](https://travis-ci.com/artem21215/neural-network-pattern-recognition-.svg?branch=master)](https://travis-ci.com/artem21215/neural-network-pattern-recognition-)
--- src/network.cpp
+++ src/network.cpp
@@ -1,8 +1,9 @@
-#include <stdio.h>
 #include <math.h>
-float sigmoid(float x){
-float z;
-  z = 1/(1 + expl(-x));
-  printf("%.10f", z);
-return z;
+#include <stdio.h>
+float sigmoid(float x)
+{
+    float z;
+    z = 1 / (1 + expl(-x));
+    printf("%.10f", z);
+    return z;
 }
