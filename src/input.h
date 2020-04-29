e8fedcdd85fa080a346635ae065de87cd4d5b41d
Artem <13_22_54@mail.ru>
2020-04-29 17:49:45 +0700

    Add a trial initialization of input 100 images

--- src/input.h
+++ src/input.h
@@ -1,5 +1,5 @@
 #ifndef _MYPROJECT_FOO_H
 #define _MYPROJECT_FOO_H
-float* input(char s[6], float arr[], int* kpixel);
+float* input(char s[17], float arr[], int* kpixel);
 void printpixel(int arr[], int n);
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,38 @@
 #include "input.h"
+#include "learn.h"
 #include "network.h"
 #include <stdio.h>
-#include "learn.h"
 int main()
 {
-    int kpixel = 0, *mas_info;
-    float *a, ***Weight, **Network,***Grad;
-    a = input("files_bmp/1.bmp", a, &kpixel);
+    int kpixel = 0, *mas_info, i;
+    float *a, ***Weight, **Network, ***Grad;
+    a = input("files_bmp/01.bmp", a, &kpixel);
     // printpixel(a, kpixel);
     Network = new float*[4];
     Network[0] = new float[kpixel];
     Network[1] = new float[16];
     Network[2] = new float[16];
     Network[3] = new float[10];
-    Network[0] = a;
+    // Network[0] = a;
     // printpixel(Network[0], kpixel);
     // RandomNetwork(kpixel);
     Weight = getW(Weight);
     mas_info = get_info(mas_info);
-    Grad=CreateGrad(Grad,mas_info);
-    printf("%d\n", result(Weight, Network, mas_info));
+    Grad = CreateGrad(Grad, mas_info);
+
+    // next: open training example
+    char s[] = "files_bmp/  .bmp";
+    int ed, des;
+    for (i = 0; i <= 99; i++) {
+        ed = i / 10;
+        des = i % 10;
+        s[10] = ed + 48;
+        s[11] = des + 48;
+        a = input(s, a, &kpixel);
+        Network[0] = a;
+        result(Weight, Network, mas_info);
+        // gradient_mas(Weight,Grad,Network,mas_info);
+        // printf("%d\n", result(Weight, Network, mas_info));
+    }
     return 0;
 }
