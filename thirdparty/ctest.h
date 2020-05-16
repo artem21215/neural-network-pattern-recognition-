5e2e3080136c5ff7db0853154b263134415ab5f1
Artem <13_22_54@mail.ru>
2020-05-17 03:02:10 +0700

    Add test for activate function(sigmoid)

---
+++ thirdparty/ctest.h
@@ -0,0 +1,19 @@
+#include "input.h"
+#include "learn.h"
+#include "network.h"
+#include <cxxtest/TestSuite.h>
+#include <iostream>
+using namespace std;
+const int n = 5;
+
+class MoveTest : public CxxTest::TestSuite {
+public:
+    void test_sigm(void)
+    {
+        TS_ASSERT_EQUALS(sigmoid(0),0.5);
+        TS_ASSERT(sigmoid(1000000)<=1);
+        TS_ASSERT(sigmoid(1000000)>0.9);
+        TS_ASSERT(sigmoid(-1000000)>=0);
+        TS_ASSERT(sigmoid(-1000000)<0.1);
+    }
+};
