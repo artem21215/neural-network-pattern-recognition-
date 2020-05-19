750516b60cff5d02814876c59a660092d0f130e6
Artem <13_22_54@mail.ru>
2020-05-20 00:07:19 +0700

    Delete difsigmoid from network.h

--- src/network.h
+++ src/network.h
@@ -6,7 +6,6 @@ float*** getW(const char s[]);
 int* get_info(const char s[]);
 int result(float*** Weight, float** Network, int* mas_info);
 float VComp(float* W, float* N, int n, float zz);
-float difsigmoid(float x);
 float cost(char* s, float** Network, int* mas_info, int N);
 float** CreateNet(int* mas_info);
 void Restruct(const char s[], float*** Weight, int* mas_info);
