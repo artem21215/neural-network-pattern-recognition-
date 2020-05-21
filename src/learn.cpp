91262230b583a887c417bccb069a9e2e66fa6665
Artem <13_22_54@mail.ru>
2020-05-22 02:24:14 +0700

    Add 2 function(learn and analysis)

--- src/learn.cpp
+++ src/learn.cpp
@@ -71,6 +71,8 @@ void learnW(
         s[14] = des + 48;
         Network[0] = input(s, &kpixel);
         t = result(Weight, Network, mas_info);
+        if (N==50)
+            printf("%d - %d\n",i%10,t);
         *sred += cost(Network, mas_info, i % 10);
         gradient_mas(Weight, Grad, Network, mas_info, i % 10);
         if (t == i % 10)
